0866314920a72d84340f5a3887dbb1cc1fa7e177
Ashiqul <meet.pollob@gmail.com>
2021-10-10 11:45:07 +0600

    |> List.find     - find by value     - find searches for the 1st value in the list

--- 2__/FindBySearching.fs
+++ 2__/FindBySearching.fs
@@ -1,6 +1,20 @@
 module FindBySearching
 
-    let numbers = [ 1 .. 2 .. 10 ]
+    let SENTINEL_VALUE = -3
 
-    printf "%A" numbers
+    let numbers = [ 1 .. 2 .. 10 ]
+    
+    let numbersWithDescription = [ (1, "ONE") ; (2, "TWO") ; (1, "THREE") ; (4, "FOUR") ; (5, "FIVE") ] 
+
+    let getByNumber sequence valueToSearch = 
+        try
+            sequence |> List.find ( fun (x) -> x = valueToSearch )
+        with
+            | :? System.Collections.Generic.KeyNotFoundException as e -> SENTINEL_VALUE
+
+    let getByKey dictionary ky = 
+        try
+            dictionary |> List.find ( fun ( x, y ) -> x = ky )
+        with
+            | :? System.Collections.Generic.KeyNotFoundException as e -> ( SENTINEL_VALUE, "SENTINEL_VALUE" )
 
--- 2__/FindBySearchingTests.fs
+++ 2__/FindBySearchingTests.fs
@@ -4,16 +4,25 @@ module Tests
     open System
     open Xunit
 
-    [<Fact>]
-    let ``My test`` () =
-        Assert.True(true)
-
-    [<Fact>]
-    let ``This should fail for no reason`` () =
-        Assert.True(false)
+    let getFromNumbers x = getByNumber numbers x
+    let getFromDic ky = getByKey numbersWithDescription ky
 
     [<Fact>]
     let ``Return numbers list which should not be empty`` () =
         Assert.True(numbers.Length > 0)
+    
+    [<Fact>]
+    let ``Must not contain 2`` () =
+        Assert.True( (getFromNumbers 2) = SENTINEL_VALUE )
 
+    [<Fact>]
+    let ``Must contain 3`` () =
+        Assert.True( (getFromNumbers 3) = 3 )
+        Assert.True( (getFromNumbers 3) <> SENTINEL_VALUE )
+
+    [<Fact>]
+    let ``Must not contain tuple with key=1 and value=THREE`` () =
+        let ( _, v ) = getFromDic 1
+        let isOne = v = "ONE"
+        Assert.True( isOne )
 
